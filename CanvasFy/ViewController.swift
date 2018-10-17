0c4b1e0fe369537c0e66e3840dab5fedd9529dfc
Tommy Tran <tommytran@tommys-macbook-pro.local>
2018-10-17 14:38:05 -0700

    Initial Commit

---
+++ CanvasFy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanvasFy
+//
+//  Created by Tommy Tran on 17/10/2018.
+//  Copyright © 2018 Tommy Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
